31c872a59ab78685558b7311f3e182aec380f7b5
Mahmoud-Emad-111 <mahmoud.devops@gmail.com>
2023-01-12 15:27:34 +0200

    source code

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio=()=>{
                 <span className="p-one">recent projects</span>
                 <span className="p-two">portfolio</span>
                 <div className="p-right">
-                    <Box img={nike} head="nike store" desc="An online store for displaying and selling shoes. Beautifully designed and responsive" link="https://nike-store-c3c4.web.app/"/>
+                    <Box img={nike} head="nike store" desc="An online store for displaying and selling shoes. Beautifully designed and responsive." link="https://nike-store-c3c4.web.app/"/>
                     <Box img={dashboard} head="Dashboard_ecommbers" link="https://dashboard-app-73c0f.web.app/" desc="Dashboard Multi-page with a beautiful design, responsive and easy to use "/>
                     <Box img={ecommerce} head="Store" desc="Online store for selling products. With a beautiful and responsive design" link="https://store-5be53.web.app"/>
                     
